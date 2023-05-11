f0b66b4059e88dd9a9dce91764dd043241f52c80
Alexis Montilla <alexisamm9261@gmail.com>
2023-05-10 21:00:27 -0400

    refactor: Base folder for models created

---
+++ app/Models/Base/Project.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Base;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'name', 'description'];
+
+    protected $perPage = 10;
+}
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,18 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Project extends Model
+final class Project extends Base\Project
 {
-    use HasFactory;
-
-    protected $fillable = ['user_id', 'name', 'description'];
-
-    protected $perPage = 10;
-
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
